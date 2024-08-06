8232c1da2c158162f2a90c1143c45fb4968d4e74
[Li Zhong Yong] <[yong@theva.com]>
2024-08-06 12:46:38 +0200

    Separator exe file created but whole line of csv read instead of only first

--- main.cpp
+++ main.cpp
@@ -1,90 +1,90 @@
-#include <iostream>
-#include <string>
-#include <fstream>
-#include <sstream>
-#include <vector>
-#include <stdint.h>
+// #include <iostream>
+// #include <string>
+// #include <fstream>
+// #include <sstream>
+// #include <vector>
+// #include <stdint.h>
 
-using namespace std;
+// using namespace std;
 
-struct MachineDataRecord {
-public:
-    MachineDataRecord(
-        string id,
-        string firstName,
-        string lastName,
-        int age,
-        string phoneNumber,
-        double gpa
-    ) {
-        Id = id;
-        FirstName = firstName;
-        LastName = lastName;
-        PhoneNumber = phoneNumber;
-        Age = age;
-        Gpa = gpa;
-    }
+// struct MachineDataRecord {
+// public:
+//     MachineDataRecord(
+//         string id,
+//         string firstName,
+//         string lastName,
+//         int age,
+//         string phoneNumber,
+//         double gpa
+//     ) {
+//         Id = id;
+//         FirstName = firstName;
+//         LastName = lastName;
+//         PhoneNumber = phoneNumber;
+//         Age = age;
+//         Gpa = gpa;
+//     }
 
-    void display() {
-        cout << "   Student ID: " << Id << endl;
-        cout << "   First Name: " << FirstName << endl;
-        cout << "    Last Name: " << LastName << endl;
-        cout << " Phone Number: " << PhoneNumber << endl;
-        cout << "          Age: " << Age << endl;
-        cout << "          GPA: " << Gpa << endl;
-        cout << endl;
-    }
+//     void display() {
+//         cout << "   Student ID: " << Id << endl;
+//         cout << "   First Name: " << FirstName << endl;
+//         cout << "    Last Name: " << LastName << endl;
+//         cout << " Phone Number: " << PhoneNumber << endl;
+//         cout << "          Age: " << Age << endl;
+//         cout << "          GPA: " << Gpa << endl;
+//         cout << endl;
+//     }
 
-    string Id;
-    string FirstName;
-    string LastName;
-    string PhoneNumber;
-    int Age;
-    double Gpa;
+//     string Id;
+//     string FirstName;
+//     string LastName;
+//     string PhoneNumber;
+//     int Age;
+//     double Gpa;
 
-};
+// };
 
-void displayStudents(vector<MachineDataRecord>& students) {
+// void displayStudents(vector<MachineDataRecord>& students) {
 
-    for (auto student : students) {
-        student.display();
-    }
-}
+//     for (auto student : students) {
+//         student.display();
+//     }
+// }
 
-int main()
-{
-    ifstream inputFile;
-    inputFile.open("C:\\temp\\student-records.csv");
-    string line = "";
+// int main()
+// {
+//     ifstream inputFile;
+//     inputFile.open("C:\\temp\\student-records.csv");
+//     string line = "";
 
-    vector<MachineDataRecord> students;
-    while (getline(inputFile, line)) {
+//     vector<MachineDataRecord> students;
+//     while (getline(inputFile, line)) {
 
-        stringstream inputString(line);
+//         stringstream inputString(line);
 
-        //StudentId, Last Name, FirstName, Age, Phone Number, GPA
-        string studentId;
-        string lastName;
-        string firstName;
-        int age;
-        string phone;
-        double gpa;
-        string tempString;
+//         //StudentId, Last Name, FirstName, Age, Phone Number, GPA
+//         string studentId;
+//         string lastName;
+//         string firstName;
+//         int age;
+//         string phone;
+//         double gpa;
+//         string tempString;
 
-        getline(inputString, studentId, ',');
-        getline(inputString, lastName, ',');
-        getline(inputString, firstName, ',');
-        getline(inputString, tempString, ',');
-        age = atoi(tempString.c_str());
-        getline(inputString, phone, ',');
-        getline(inputString, tempString);
-        gpa = atof(tempString.c_str());
+//         getline(inputString, studentId, ',');
+//         getline(inputString, lastName, ',');
+//         getline(inputString, firstName, ',');
+//         getline(inputString, tempString, ',');
+//         age = atoi(tempString.c_str());
+//         getline(inputString, phone, ',');
+//         getline(inputString, tempString);
+//         gpa = atof(tempString.c_str());
 
-        MachineDataRecord student(studentId, lastName,
-            firstName, age, phone, gpa);
-        students.push_back(student);
-        line = "";
-    }
+//         MachineDataRecord student(studentId, lastName,
+//             firstName, age, phone, gpa);
+//         students.push_back(student);
+//         line = "";
+//     }
 
-    displayStudents(students);
-}
+//     displayStudents(students);
+// }
